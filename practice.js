1bf9cbd5b98e7a13648979c162a6bfffa52ec1c7
Gunika Tulsyan <tulsyangunika@gmail.com>
2025-05-20 14:42:29 +0545

    enum

--- practice.js
+++ practice.js
@@ -1,9 +1,9 @@
-var Mess = "Hello, TypeScript";
-console.log(Mess);
-var Ag = 25;
-var Nam = "Alice";
-var IsActiv = true;
-console.log(Ag, Nam, IsActiv);
-var fruits = ["apple", "banana"];
-var user = ["Alice", 25];
-console.log(fruits, user);
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+var move = Direction.Up;
+console.log(move);
--- practice.ts
+++ practice.ts
@@ -1,11 +1,9 @@
-const Mess: string = "Hello, TypeScript";
-console.log(Mess);
-
-let Ag: number =25;
-let Nam: string ="Alice";
-let IsActiv: boolean = true;
-console.log(Ag, Nam, IsActiv);
-
-let fruits: string[] = ["apple", "banana"];
-let user: [string, number] = ["Alice", 25];
-console.log(fruits, user);
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right
+}
+let move: Direction = Direction.Up;
+console.log(move);
+ 
